1ba118179663eebe62c352bff5a7784c53cd66af
David Liu <david.liu@yardi.com>
2023-07-30 22:49:20 -0600

    Make it compilable in Java 19

--- src/test/java/spark/WebSocketRequestTest.java
+++ src/test/java/spark/WebSocketRequestTest.java
@@ -45,6 +45,7 @@ public class WebSocketRequestTest {
         get(HELLO, (req, res) -> "Hello!");
         get(OTHER, (req, res) -> "other");
         init();
+        awaitInitialization();
     }
 
     @Test
@@ -135,27 +136,3 @@ class Chat{
     }
 }
 
-@WebSocket
-class ChatWebSocketHandler {
-
-    private String sender, msg;
-
-    @OnWebSocketConnect
-    public void onConnect(Session user) throws Exception {
-        String username = "User" + Chat.nextUserNumber++;
-        Chat.userUsernameMap.put(user, username);
-        Chat.broadcastMessage(sender = "Server", msg = (username + " joined the chat"));
-    }
-
-    @OnWebSocketClose
-    public void onClose(Session user, int statusCode, String reason) {
-        String username = Chat.userUsernameMap.get(user);
-        Chat.userUsernameMap.remove(user);
-        Chat.broadcastMessage(sender = "Server", msg = (username + " left the chat"));
-    }
-
-    @OnWebSocketMessage
-    public void onMessage(Session user, String message) {
-        Chat.broadcastMessage(sender = Chat.userUsernameMap.get(user), msg = message);
-    }
-}
